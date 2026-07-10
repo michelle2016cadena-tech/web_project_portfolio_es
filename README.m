088ff260e8ee3c2fece5c52022028fa35a91c72f
michelle2016cadena-tech <michelle2016cadena@gmail.com>
2026-07-09 19:56:26 -0500

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,16 @@
+# web_project_potrfolio_es
+
+## Descripcion breve del proyecto
+Portafolio web profesional de Erika Cadena, desarrolladora web especializada en HTML, CSS y JavaScript. La interfaz presenta un diseño moderno y elegante con una paleta de colores oscuros y detalles luminosos, destacando una fotografía profesional, una sección de habilidades técnicas y un apartado de proyectos desarrollados. El sitio está diseñado para mostrar experiencia en la creación de interfaces web limpias, responsivas y funcionales, además de facilitar el contacto profesional a través de diferentes canales digitales.
+
+## Tecnologias Usadas
+* HTML5
+* CSS3
+* JavaScript
+
+## Como se Uso en el Proyecto
+1. clonacion de repositorios
+2. archivos index.html
+
+## Autor
+Erika cadena
